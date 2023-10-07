9884c0c31ce23f4cb09a24288ace02b985acb73b
AlexeyKuchun <alexey.kuchun@gmial.com>
2023-10-07 16:01:45 +0200

    Adding task from the first lesson

---
+++ src/HomeWork/Lesson1/Task.java
@@ -0,0 +1,17 @@
+package HomeWork.Lesson1;
+
+/*1. Написать программу для вывода «Hello <user>» на консоль.
+Пусть <user> вводиться с консоли, программа должна потребовать ввод <user>.
+Для чтения с консоли используйте класс Scanner.*/
+
+import java.util.Scanner;
+public class Task
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please, enter User name: ");
+        String user = in.nextLine();
+        System.out.print("Hello " + user + "!");
+        in.close();
+    }
+}
